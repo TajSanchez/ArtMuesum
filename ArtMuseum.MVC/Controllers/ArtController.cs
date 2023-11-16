729dc72e9d572b7da7238884495929ceea5b9d6c
Taj <tajmarie92@gmail.com>
2023-11-15 22:14:59 -0600

    Added MVC portion of project. Was able to get first random generated image displayed on the "Art" tab on the Webpage.

---
+++ ArtMuseum.MVC/Controllers/ArtController.cs
@@ -0,0 +1,27 @@
+﻿using ArtMuseumLibrary;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ArtMuseum.MVC.Controllers
+{
+    public class ArtController : Controller
+    {
+        private const string BASE_URL = $"https://collectionapi.metmuseum.org/public/collection/v1/objects";
+
+        public IActionResult Index()
+        {
+            
+            BaseServiceCall baseServiceCall = new BaseServiceCall(new HttpClient());
+            Random random = new Random();
+
+            List<int> artIds = baseServiceCall.GetArtIds(BASE_URL);
+
+            int index = random.Next(artIds.Count);
+
+            var artUrl = $"{BASE_URL}/{artIds[index]}";
+
+            var artPiece = baseServiceCall.GetArtPiece(artUrl);
+            return View(artPiece);
+        }
+    }
+}
